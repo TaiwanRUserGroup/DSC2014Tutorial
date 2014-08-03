9043ca0009abf85090babf72dc8bb2b4f55bf21e
Wush Wu <wush978@gmail.com>
2014-08-03 16:01:19 +0800

    [test] knit slides update to 3.0.0

---
+++ tests/testKnit.R
@@ -0,0 +1,8 @@
+library(knitr)
+slides <- c("Basic/slides/index.Rmd", "ETL/slides/index.Rmd", "Visualization/slide1/index.Rmd", "Visualization//slide2/lecture_ggplot2/index.Rmd", "Visualization//slide3/index.Rmd")
+slides <- system.file(slides, package="DSC2014Tutorial")
+stopifnot(slides != "")
+out.file <- list()
+for(slide in slides) {
+  knit(slide, out.file[[slide]] <- tempfile(fileext = ".md"))
+}
